3f73c11da8dd66cf94619209de09255b917fc1db
LePirlouit <lepirlouit@hotmail.com>
2015-01-20 15:23:05 +0100

    add method getFederationByCode in FederationDao

--- am-bus-impl/src/main/java/be/pir/am/api/dao/FederationDao.java
+++ am-bus-impl/src/main/java/be/pir/am/api/dao/FederationDao.java
@@ -6,5 +6,5 @@ import be.pir.am.entities.FederationEntity;
 
 @Local
 public interface FederationDao extends EntityDao<FederationEntity> {
-
+	FederationEntity getByCode(String code);
 }
--- am-bus-impl/src/main/java/be/pir/am/dao/FederationDaoImpl.java
+++ am-bus-impl/src/main/java/be/pir/am/dao/FederationDaoImpl.java
@@ -1,6 +1,10 @@
 package be.pir.am.dao;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import be.pir.am.api.dao.FederationDao;
 import be.pir.am.entities.FederationEntity;
@@ -11,6 +15,18 @@ public class FederationDaoImpl extends AbstractEntityDao<FederationEntity>
 
 	public FederationDaoImpl() {
 		super(FederationEntity.class);
+	}
+
+	@Override
+	public FederationEntity getByCode(String code) {
+		EntityManager em = getEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<FederationEntity> cq = cb.createQuery(FederationEntity.class);
+		Root<FederationEntity> federation = cq.from(FederationEntity.class);
+
+		cq.where(cb.equal(federation.get("code"), code));
+		return em.createQuery(cq).getSingleResult();
 	}
 
 }
