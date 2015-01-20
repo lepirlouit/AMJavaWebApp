f15dba9c061dd10aad51e94970d03965133c4e83
LePirlouit <lepirlouit@hotmail.com>
2015-01-20 15:26:41 +0100

    add FederationDaoImplTest

---
+++ am-bus-impl/src/test/java/be/pir/am/dao/FederationDaoImplTest.java
@@ -0,0 +1,36 @@
+package be.pir.am.dao;
+
+import junit.framework.Assert;
+import be.pir.am.entities.AthleteEntity;
+import be.pir.am.entities.CategoryEntity;
+import be.pir.am.entities.CompetitionEntity;
+import be.pir.am.entities.CompetitorEntity;
+import be.pir.am.entities.CountryEntity;
+import be.pir.am.entities.EventEntity;
+import be.pir.am.entities.FederationEntity;
+import be.pir.am.entities.LicenceEntity;
+import be.pir.am.entities.ParticipationEntity;
+import be.pir.am.entities.RoundEntity;
+import be.pir.am.entities.TeamEntity;
+
+import com.bm.testsuite.BaseSessionBeanFixture;
+
+public class FederationDaoImplTest extends BaseSessionBeanFixture<FederationDaoImpl> {
+	private static final Class<?>[] usedBeans = { AthleteEntity.class, CountryEntity.class, LicenceEntity.class,
+			TeamEntity.class, FederationEntity.class, CategoryEntity.class, CompetitionEntity.class,
+			CompetitorEntity.class, EventEntity.class, ParticipationEntity.class, RoundEntity.class };
+
+	/**
+	 * Constructor.
+	 */
+	public FederationDaoImplTest() {
+		super(FederationDaoImpl.class, usedBeans);
+	}
+
+	public void testMaxOneCategoryForEachAge() {
+		final FederationDaoImpl toTest = this.getBeanToTest();
+
+		FederationEntity federation = toTest.getByCode("BEL_LBFA");
+		Assert.assertNotNull(federation);
+	}
+}
