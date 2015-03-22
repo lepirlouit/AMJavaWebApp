0d1a67f66e1505095439df18dbb8471d59404c0b
Benoit de Biolley <me@lepirlouit.com>
2015-03-22 10:31:57 +0100

    pure bootstrap

--- am-web-wicket/src/main/java/be/pir/HomePage.java
+++ am-web-wicket/src/main/java/be/pir/HomePage.java
@@ -3,7 +3,7 @@ package be.pir;
 import javax.ejb.EJB;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import be.pir.am.api.service.AthleteService;
@@ -17,9 +17,17 @@ public class HomePage extends WebPage {
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+		//		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+		Form<?> form = new Form("searchform") {
+			@Override
+			protected void onSubmit() {
+				System.out.println("Form submitted.");
+			}
+		};
+		add(form);
+
 		athleteService.findAthletesByBib(1997);
 		// TODO Add your page's components here
 
-    }
+	}
 }
